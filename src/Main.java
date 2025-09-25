10f4a65d0eacab8bed8860b4c25eb24131a44f81
Glover69 <iamglover02@gmail.com>
2025-09-25 12:39:10 +0000

    2nd assignment

--- src/Main.java
+++ src/Main.java
@@ -3,57 +3,68 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner userInput = new Scanner(System.in);
+//        Scanner userInput = new Scanner(System.in);
+//
+//        String majorAndStatus;
+//        char userMajor;
+//        char userStatus;
+//
+//        System.out.println("Enter the two characters for your major and status: ");
+//        majorAndStatus = userInput.nextLine();
+//
+//        userMajor = majorAndStatus.charAt(0);
+//
+//        userStatus = majorAndStatus.charAt(1);
+//
+//        String course = "";
+//        String status = "";
+//
+//        switch (userMajor){
+//            case 'M', 'm':
+//                course = "Mathematics";
+//                break;
+//            case 'C', 'c':
+//                course = "Computer Science";
+//                break;
+//            case 'I', 'i':
+//                course = "Information Technology";
+//                break;
+//            default:
+//                System.out.println("Course not found");
+//                break;
+//        }
+//
+//        switch (userStatus){
+//            case '1':
+//                status = "Freshman";
+//                break;
+//            case '2':
+//                status = "Sophomore";
+//                break;
+//            case '3':
+//                status = "Junior";
+//                break;
+//            case '4':
+//                status = "Senior";
+//                break;
+//            default:
+//                System.out.println("Year group not found");
+//                break;
+//        }
+//
+//        System.out.println(course + " " + status);
 
-        String majorAndStatus;
-        char userMajor;
-        char userStatus;
 
-        System.out.println("Enter the two characters for your major and status: ");
-        majorAndStatus = userInput.nextLine();
+        Scanner scanner = new Scanner(System.in);
 
-        userMajor = majorAndStatus.charAt(0);
+        System.out.println("Enter the radius: ");
+        double radius = scanner.nextDouble();
 
-        userStatus = majorAndStatus.charAt(1);
+        final double PI = 3.1459;
+        double side = Math.sin(PI/5) *2*radius;
+        double area = (5 * Math.pow(side,2)) / (4* Math.tan(PI/5));
 
-        String course = "";
-        String status = "";
-
-        switch (userMajor){
-            case 'M', 'm':
-                course = "Mathematics";
-                break;
-            case 'C', 'c':
-                course = "Computer Science";
-                break;
-            case 'I', 'i':
-                course = "Information Technology";
-                break;
-            default:
-                System.out.println("Course not found");
-                break;
-        }
-
-        switch (userStatus){
-            case '1':
-                status = "Freshman";
-                break;
-            case '2':
-                status = "Sophomore";
-                break;
-            case '3':
-                status = "Junior";
-                break;
-            case '4':
-                status = "Senior";
-                break;
-            default:
-                System.out.println("Year group not found");
-                break;
-        }
-
-        System.out.println(course + " " + status);
-
+        System.out.printf("Area: ", area);
 
 
     }
